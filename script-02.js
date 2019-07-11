c3139394fb4ab7874537bbe5be598dc6c0cd21a8
Felipe Teixeira Moraes <fmoraes@mastertech.tech>
2019-07-11 11:10:47 -0300

    Adicionar exemplos de lista, objeto e while. Adicionar exercício de while

---
+++ script-02.js
@@ -0,0 +1,43 @@
+let vetor = ['Felipe', 'Carol', 'Lolo', false, 12, 256, -1000];
+
+let joaozinho = {
+    nome: 'Joãozinho',
+    sobrenome: 'Das Neves',
+    idade: 16,
+    pets: [
+        {
+            nome: 'Rex',
+            raca: 'Vira-lata'
+        },
+        {
+            nome: 'Spike',
+            raca: 'Tomba-potes'
+        }
+    ]
+};
+
+let mariazinha = {
+    nome: 'Maria',
+    sobrenome: 'Zinha',
+    idade: 23,
+    pets: []
+};
+
+// console.log('Entrou no laço');
+
+// CONTADOR
+let i = 10;
+// CONDIÇÃO
+while(i > 0) {
+    console.log(i);
+
+    // PASSO
+    i = i - 1;
+    i -= 1;
+    i--;
+    //contador = contador + 1;
+    //contador += 1;
+    //contador++;
+}
+
+console.log('Vamos pular!');
--- script.js
+++ script.js
@@ -1,34 +1,22 @@
-/*
-Nós vamos criar um sistema que avalia se o aluno foi aprovado ou não em uma matéria. Pergunte pro usuário:
-- O nome do aluno
-- A matéria
-- A nota
-Com os valores recebidos, verifique se o aluno teve nota superior a média (6). A saída do programa deve falar:
-"Parabéns, o aluno X foi aprovado na matéria Y"
-OU
-"Parece que a nota do aluno X na matéria Y foi insuficiente"
+/* 
+Crie um sistema onde o usuário indica quantos lances de escada ele gostaria de criar. Com esse valor, desenhe as escadas no console.
+
+Exemplo:
+Usuário digita 5 lances
+*
+**
+***
+****
+*****
 
 BÔNUS:
-Crie uma segunda condição onde o aluno é aprovado "pelo sistema" se a nota for 0.5 abaixo da média. Nesse caso, o programa deve falar:
-"O aluno X foi aprovado na matéria Y pelo sistema."
+Permita que o usuário defina qual o 'material' da escada.
+
+Exemplo:
+Usuário quer 5 lances de escada utilizando #
+#
+##
+###
+####
+#####
 */
-
-let aluno = prompt('Qual o nome do aluno?');
-let materia = prompt('Qual o nome da matéria?');
-let nota = prompt('E qual foi a nota?');
-
-let media = 6;
-
-if (Number(nota) >= media) {
-    // alert('Parabéns, o aluno '+ aluno + ' foi aprovado na matéria ' + materia);
-    //Template String
-    alert(`Parabéns, o aluno ${aluno} foi aprovado na matéria ${materia}`);
-}
-else if (Number(nota) >= media - 0.5) {
-    // alert('O aluno '+ aluno + ' foi aprovado na matéria ' + materia + ' pelo sistema');
-    alert(`O aluno ${aluno} foi aprovado na matéria ${materia} pelo sistema`);
-}
-else {
-    // alert('Parece que a nota do aluno ' + aluno + ' na matéria ' + materia + ' foi insuficiente');
-    alert(`Parece que a nota do aluno ${aluno} na matéria ${materia} foi insuficiente`);
-}
