2875adc18f086f9ecee88d1d7c255d3cb9412145
Felipe Teixeira Moraes <fmoraes@mastertech.tech>
2019-07-11 12:04:06 -0300

    Adicionar solução do exercício da escada

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ Usuário quer 5 lances de escada utilizando #
 ####
 #####
 */
+
+let lancesDeEscada = prompt('Quantos lances de escada você gostaria?');
+
+while(isNaN(Number(lancesDeEscada))) {
+    lancesDeEscada = prompt(`Você digitou "${lancesDeEscada}". O programa só aceita valores numéricos nessa etapa. Por favor, digite quantos lances de escada você gostaria`);
+}
+
+let material = prompt('Qual o material da sua escada?');
+let desenho = '';
+
+let i = 0;
+
+while (i < Number(lancesDeEscada)) {
+    desenho = desenho + material;
+    console.log(desenho);
+
+    i++;
+}
